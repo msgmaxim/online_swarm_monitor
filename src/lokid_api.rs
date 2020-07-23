747eee84caefc7f00dc41f2155690b36064e1541
Maxim Shishmarev <msgmaxim@gmail.com>
2020-07-23 16:46:01 +1000

    Get node list from seed

---
+++ src/lokid_api.rs
@@ -0,0 +1,56 @@
+
+use serde_json::{json, Value};
+
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct ServiceNodeRecord {
+    pub public_ip: String,
+    pub storage_port: u16,
+    pub storage_lmq_port: u16,
+    pub pubkey_x25519: String,
+    pub pubkey_ed25519: String,
+    pub swarm_id: u64,
+}
+
+pub struct Network {
+    pub seed_url: &'static str,
+    pub is_testnet: bool,
+}
+
+pub async fn get_n_service_nodes(limit: u32, network: &Network) -> Vec<ServiceNodeRecord> {
+    let client = reqwest::Client::new();
+
+    let params = json!({
+        "jsonrpc": "2.0",
+        "id": "0",
+        "method": "get_n_service_nodes",
+        "params": {
+            "limit": &limit,
+            "fields": {
+                "public_ip": true,
+                "storage_port": true,
+                "storage_lmq_port": true,
+                "pubkey_x25519": true,
+                "pubkey_ed25519": true,
+                "swarm_id": true,
+            },
+            "active_only": false,
+        },
+    });
+
+    let res = client
+        .post(network.seed_url)
+        .json(&params)
+        .send()
+        .await
+        .expect("Failed to send get_n_service_nodes");
+
+    let res_text = res.text().await.expect("obtaining text from response");
+
+    let v: Value = serde_json::from_str(&res_text).expect("parsing json");
+
+    let array = &v["result"]["service_node_states"];
+
+    serde_json::from_value(array.clone()).expect("from json to value")
+}
---
+++ src/main.rs
@@ -0,0 +1,46 @@
+
+mod node_pool;
+mod lokid_api;
+
+use lokid_api::Network;
+
+const FOUNDATION_TESTNET_SEED: &'static str = "http://public.loki.foundation:38157/json_rpc";
+const FOUNDATION_MAINNET_SEED: &'static str = "http://public.loki.foundation:22023/json_rpc";
+
+#[tokio::main]
+async fn main() {
+
+    use clap::{App, Arg};
+
+    let matches = App::new("online-swarm-monitor")
+        .version("0.1")
+        .arg(Arg::with_name("net").takes_value(true)).get_matches();
+
+    let testnet = Network {
+        seed_url: FOUNDATION_TESTNET_SEED,
+        is_testnet: true,
+    };
+
+    let mainnet = Network {
+        seed_url: FOUNDATION_MAINNET_SEED,
+        is_testnet: false,
+    };
+
+    let network = match matches.value_of("net") {
+        Some(n) => {
+            if n == "net=testnet" {
+                testnet
+            } else if n == "net=mainnet" {
+                mainnet
+            } else {
+                mainnet
+            }
+        }
+        None => mainnet,
+    };
+
+    let node_pool = lokid_api::get_n_service_nodes(2000, &network).await;
+
+    dbg!(node_pool.len());
+
+}
